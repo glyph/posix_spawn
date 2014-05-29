20f677d2dd4b084ed90d8dbb2978428e310df1e9
David Reid <dreid@dreid.org>
2014-05-29 16:46:51 -0700

    The House Committee on Unpythonic Activities.

--- posix_spawn/_lib.py
+++ posix_spawn/_lib.py
@@ -5,8 +5,8 @@ from cffi import FFI
 
 ffi = FFI()
 
-ffi.cdef(pkgutil.get_data('posix_spawn', 'c/cdef.h'))
+ffi.cdef(pkgutil.get_data('posix_spawn', 'c/cdef.h').decode('ascii'))
 
-lib = ffi.verify(pkgutil.get_data('posix_spawn', 'c/verify.h'))
+lib = ffi.verify(pkgutil.get_data('posix_spawn', 'c/verify.h').decode('ascii'))
 
 __all__ = ('lib', 'ffi')
